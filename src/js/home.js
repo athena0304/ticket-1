82c78fac0ba1566d0a46009146ac948c1327367b
dongzhe <dongzhe@360.cn>
2016-06-06 21:19:35 +0800

    code modefied

--- src/js/home.js
+++ src/js/home.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
         $(this).css("left", index * 3.33 + "%")
     })
 
-    var num = 15;
+    var num = 20 - parseInt($(".remain_cheer_num").eq(0).text());
 
-    ticket_models.filter(":lt(5)").addClass('hide')
+    ticket_models.filter(":lt("+ num +")").addClass('hide')
 });
