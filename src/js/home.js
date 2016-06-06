3396c42b96962ee1612610a6a955701e7ca0f116
ouxuewen <ouxuewen@360.cn>
2016-06-06 23:15:49 +0800

    modify

--- src/js/home.js
+++ src/js/home.js
@@ -1,14 +1,4 @@
 $(document).ready(function() {
-    // $(".ticket_modal").height($(".ticket_img_wrapper").height());
-    var ticket_models = $(".ticket_modal")
-    ticket_models.each(function(index, ele) {
-        $(this).css("left", index * 3.33 + "%")
-    })
-
-    var num = 20 - parseInt($(".remain_cheer_num").eq(0).text());
-
-    ticket_models.filter(":lt("+ num +")").addClass('hide');
-
     $(".info").on("click", function(){
         layer.open({
             title: [
