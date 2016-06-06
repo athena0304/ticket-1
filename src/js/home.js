1173c2397b5f231609ba2dd5451540e539e02d76
ouxuewen <ouxuewen@360.cn>
2016-06-06 23:38:25 +0800

    modify

--- src/js/home.js
+++ src/js/home.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-    $(".info").on("click", function(){
+
+    function loadQrcode () {
         layer.open({
             title: [
             '长按关注官方账号',
@@ -7,6 +8,14 @@ $(document).ready(function() {
             ],
             content:"<img src='/static/img/qrcode.png'></img><p>长按关注G-STEPS街舞社可随时咨询问题</p>",
         })
+    }
+
+    $(".info").on("click", function(){
+        loadQrcode();
+    })
+
+    $(".footer a").on("click", function() {
+        loadQrcode();
     })
 
 });
