7768e331b6795c186bd9e25e2ceed298187232d6
guoguangchuan <ggc0402@qq.com>
2016-07-12 20:57:05 +0800

    fix bug

--- tools/tool.py
+++ tools/tool.py
@@ -2,11 +2,11 @@
 __author__ = 'guoguangchuan'
 
 
-from tools.global_conf import satisfy_cheer_num
+from tools.global_conf import *
 
 
 def get_remain_cheer_num(cheer_num):
-    remain_cheer_num = satisfy_cheer_num - cheer_num
+    remain_cheer_num = SATISFY_CHEER_NUM - cheer_num
     if remain_cheer_num < 0:
         remain_cheer_num = 0
     return remain_cheer_num
